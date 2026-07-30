8c9e6be5225481fc3e12c2d7d21cf0d289f3d897
Larry Oliveira <larry.d.de.oliveira@accenture.com>
2026-07-30 16:23:24 -0300

    adiciona projeto inicial criado pelo spring initializr

---
+++ src/test/java/br/com/larry/games/GamesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.larry.games;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
